351d670785f7bbb89b999a6d7f9bc9d9cfce120e
= <79530088804@yandex.ru>
2025-07-26 11:48:57 +0500

    Creating an admin and a user pages

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+app_name = 'account'
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('favorite/', views.favorite, name='favorite'),
+    path('register/', views.register, name='register'),
+]
